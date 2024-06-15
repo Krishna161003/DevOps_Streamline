143deb980d5d95c18e47c3713c9751bb1da5b686
Krishna161003 <132978171+Krishna161003@users.noreply.github.com>
2024-06-15 13:54:51 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-
-        #!/bin/bash
-
-        # Start Gunicorn
-        gunicorn DevOps_Streamline.wsgi:application --bind 0.0.0.0:8000 --workers 3 &
-
-        # Start Nginx
-        nginx -g 'daemon off;'
-        
